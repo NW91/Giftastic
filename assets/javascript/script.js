70e67820ad74e4d57a3fe2b66c6e7f4ce32ee984
Nicole W <nicoleewhitee@gmail.com>
2019-01-26 00:57:56 -0800

    formatting in the html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,26 @@
-$("button").on("click", function() {
+$(document).ready(function(){
 
-var topics [tennis, swimming, basketball, soccer, football, hockey];
-};
+//Starting array of sports
+var sportTopics = [tennis, swimming, basketball, soccer, football, hockey]
+
+designatedLocation = " "
+
+//function for showing the sports
+function renderButtons() {
+
+//clearing the search button
+$("#sports-view").empty();
+
+//for loop of the sports + creating new buttons/classes
+for(var i = 0; i < sportTopics.length; i++) {
+var a = $('<button>');
+a.addClass('sport');
+
+a.attr('data-name', sportTopics[i]);
+a.text(sportTopics[i]);
+$("sports-view").append(a);
+
+    }
+}
+
+})
