0132b9520f303141efc641fa8a1005b1804e2422
Nicole W <nicoleewhitee@gmail.com>
2019-01-26 10:16:47 -0800

    event listeners added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,13 +5,13 @@ var sportTopics = [tennis, swimming, basketball, soccer, football, hockey]
 
 designatedLocation = " "
 
-//function for showing the sports
+//Function for showing the sports
 function renderButtons() {
 
-//clearing the search button
+//Clearing the search button
 $("#sports-view").empty();
 
-//for loop of the sports + creating new buttons/classes
+//Forloop of the sports + creating new buttons/classes
 for(var i = 0; i < sportTopics.length; i++) {
 var a = $('<button>');
 a.addClass('sport');
@@ -19,8 +19,26 @@ a.addClass('sport');
 a.attr('data-name', sportTopics[i]);
 a.text(sportTopics[i]);
 $("sports-view").append(a);
-
     }
-}
+s= $("sports-input").focus();
+    }
+
+renderButtons();
+
+//Event listeners for Buttons
+$("#add-sport").on('click', function() {
+
+//Event listener that lets the user hit enter functionality
+event.preventDefault();
+    
+//Takes the text from the input box
+var differentSport = $("sport-input").val().trim();
+    
+//Sport input from the textbox added to the array
+topics.push(differentSport);
+    
+//Calls the button which handles the processing of our topics sports array
+renderButtons();
+    });
 
 })
